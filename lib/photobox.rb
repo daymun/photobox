0b464ad3176495f27e603dc2c77ec29da416329a
Devin Wadsworth <devinwadsworth@gmail.com>
2014-11-02 01:17:40 -0700

    add executable and update photo class

--- lib/photobox.rb
+++ lib/photobox.rb
@@ -1,4 +1,3 @@
-#!/usr/bin/env ruby
 require "photobox/version"
 require "fileutils"
 
@@ -8,40 +7,44 @@ module Photobox
     SCREENSHOTS_DIR = "#{PHOTOS_DIR}/Screenshots"
     VIDEOS_DIR      = "#{PHOTOS_DIR}/Videos"
 
-    def self.cluster
-      cluster_photos
-      cluster_screenshots
-      cluster_videos
-    end
-
-    def cluster_photos
-      Dir.glob("#{PHOTOS_DIR}/*.jpg") do |photo|
-        photo_date = photo.split("/")[-1].split(" ")[0]
-        next if defined?(last_photo_date) && photo_date == last_photo_date
-
-        photo_month_dir = "#{PHOTOS_DIR}/#{photo_date}"
-        puts "Creating directory: #{photo_month_dir}"
-        FileUtils::mkdir_p(photo_month_dir)
-        FileUtils.mv(Dir.glob("#{photo_month_dir}*.jpg"), photo_month_dir)
-        last_photo_date = photo_date
+    class << self
+      def cluster
+        cluster_photos
+        cluster_screenshots
+        cluster_videos
       end
-    end
 
-    def cluster_screenshots
-      screenshots = Dir.glob("#{PHOTOS_DIR}/*.png")
-      unless screenshots.count == 0
-        puts "Moving screenshots to #{SCREENSHOTS_DIR}"
-        FileUtils.mkdir_p(SCREENSHOTS_DIR)
-        FileUtils.mv(screenshots, SCREENSHOTS_DIR)
+      protected
+
+      def cluster_photos
+        Dir.glob("#{PHOTOS_DIR}/*.jpg") do |photo|
+          photo_date = photo.split("/")[-1].split(" ")[0]
+          next if defined?(last_photo_date) && photo_date == last_photo_date
+
+          photo_month_dir = "#{PHOTOS_DIR}/#{photo_date}"
+          puts "Creating directory: #{photo_month_dir}"
+          FileUtils::mkdir_p(photo_month_dir)
+          FileUtils.mv(Dir.glob("#{photo_month_dir}*.jpg"), photo_month_dir)
+          last_photo_date = photo_date
+        end
       end
-    end
 
-    def cluster_videos
-      videos = Dir.glob("#{PHOTOS_DIR}/*.{mov,mp4}")
-      unless videos.count == 0
-        puts "Moving videos to #{VIDEOS_DIR}"
-        FileUtils.mkdir_p(VIDEOS_DIR)
-        FileUtils.mv(videos, VIDEOS_DIR)
+      def cluster_screenshots
+        screenshots = Dir.glob("#{PHOTOS_DIR}/*.png")
+        unless screenshots.count == 0
+          puts "Moving screenshots to #{SCREENSHOTS_DIR}"
+          FileUtils.mkdir_p(SCREENSHOTS_DIR)
+          FileUtils.mv(screenshots, SCREENSHOTS_DIR)
+        end
+      end
+
+      def cluster_videos
+        videos = Dir.glob("#{PHOTOS_DIR}/*.{mov,mp4}")
+        unless videos.count == 0
+          puts "Moving videos to #{VIDEOS_DIR}"
+          FileUtils.mkdir_p(VIDEOS_DIR)
+          FileUtils.mv(videos, VIDEOS_DIR)
+        end
       end
     end
   end
--- lib/photobox/cli.rb
+++ lib/photobox/cli.rb
@@ -3,7 +3,7 @@ require "photobox"
 
 module Photobox
   class CLI < Thor
-    desc "Group photos into folders by month"
+    desc "cluster", "Group photos into folders by month"
     def cluster
       Photobox::Photo.cluster
     end
