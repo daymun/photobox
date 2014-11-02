db389c0da4ca55653013da7a92f292f112b1ea8a
Devin Wadsworth <devinwadsworth@gmail.com>
2014-11-01 18:44:57 -0700

    move photos to their respective month folders

--- photobox.rb
+++ photobox.rb
@@ -11,5 +11,6 @@ Dir.glob("#{PHOTOS_DIR}/*.jpg") do |photo|
   photo_month_dir = "#{PHOTOS_DIR}/#{photo_date}"
   puts "Creating directory: #{photo_month_dir}"
   FileUtils::mkdir_p("#{photo_month_dir}")
+  FileUtils.mv(Dir.glob("#{photo_month_dir}*.jpg"), photo_month_dir)
   last_photo_date = photo_date
 end
