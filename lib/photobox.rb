c698db59257260974b735e57f1eab8578ac97155
Devin Wadsworth <devinwadsworth@gmail.com>
2014-11-02 01:45:08 -0700

    pass files as args

--- lib/photobox.rb
+++ lib/photobox.rb
@@ -9,38 +9,53 @@ module Photobox
 
     class << self
       def cluster
-        cluster_photos
-        cluster_screenshots
-        cluster_videos
+        files       = Dir.glob("#{PHOTOS_DIR}/*.*")
+        photos      = Dir.glob("#{PHOTOS_DIR}/*.jpg")
+        screenshots = Dir.glob("#{PHOTOS_DIR}/*.png")
+        videos      = Dir.glob("#{PHOTOS_DIR}/*.{mov,mp4}")
+
+        if files.count.zero?
+          puts "No unclustered files."
+        else
+          cluster_photos(photos)
+          cluster_screenshots(screenshots)
+          cluster_videos(videos)
+        end
       end
 
       protected
 
-      def cluster_photos
-        Dir.glob("#{PHOTOS_DIR}/*.jpg") do |photo|
-          photo_date = photo.split("/")[-1].split(" ")[0]
-          next if defined?(last_photo_date) && photo_date == last_photo_date
+      def cluster_photos(photos)
+        if photos.count.zero?
+          puts "No unclustered photos."
+        else
+          photos.each do |photo|
+            photo_date = photo.split("/")[-1].split(" ")[0]
+            next if defined?(last_photo_date) && photo_date == last_photo_date
 
-          photo_month_dir = "#{PHOTOS_DIR}/#{photo_date}"
-          puts "Creating directory: #{photo_month_dir}"
-          FileUtils::mkdir_p(photo_month_dir)
-          FileUtils.mv(Dir.glob("#{photo_month_dir}*.jpg"), photo_month_dir)
-          last_photo_date = photo_date
+            photo_month_dir = "#{PHOTOS_DIR}/#{photo_date}"
+            puts "Creating directory: #{photo_month_dir}"
+            FileUtils::mkdir_p(photo_month_dir)
+            FileUtils.mv(Dir.glob("#{photo_month_dir}*.jpg"), photo_month_dir)
+            last_photo_date = photo_date
+          end
         end
       end
 
-      def cluster_screenshots
-        screenshots = Dir.glob("#{PHOTOS_DIR}/*.png")
-        unless screenshots.count == 0
+      def cluster_screenshots(screenshots)
+        if screenshots.count.zero?
+          puts "No unclustered screenshots."
+        else
           puts "Moving screenshots to #{SCREENSHOTS_DIR}"
           FileUtils.mkdir_p(SCREENSHOTS_DIR)
           FileUtils.mv(screenshots, SCREENSHOTS_DIR)
         end
       end
 
-      def cluster_videos
-        videos = Dir.glob("#{PHOTOS_DIR}/*.{mov,mp4}")
-        unless videos.count == 0
+      def cluster_videos(videos)
+        if videos.count.zero?
+          puts "No unclustered videos."
+        else
           puts "Moving videos to #{VIDEOS_DIR}"
           FileUtils.mkdir_p(VIDEOS_DIR)
           FileUtils.mv(videos, VIDEOS_DIR)
