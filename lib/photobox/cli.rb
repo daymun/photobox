2ae32788d4e1fec698527146b2af8465a75d2995
Devin Wadsworth <devinwadsworth@gmail.com>
2014-11-02 03:51:03 -0800

    default info task

--- lib/photobox/cli.rb
+++ lib/photobox/cli.rb
@@ -3,6 +3,8 @@ require "photobox"
 
 module Photobox
   class CLI < Thor
+    default_task :info
+
     desc "cluster", "Group photos into folders by month"
     def cluster
       Photobox::Photo.cluster
